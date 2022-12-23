827c493d7ffcfeb8e318164ae34ad97f6a5a5044
Thinh Bui <thinhbui2911@gmail.com>
2022-12-22 16:02:20 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+RESPONSES_KEY = "responses"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret key"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+@app.route("/")
+def show_survey_start():
+    return render_template("survey_start.html", survey = survey)
+
+@app.route("/begin", methods=["POST"])
+def start_survey():
+    session[RESPONSES_KEY] = []
+    return redirect("/questions/0")
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    choice = request.form['answer']
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    responses = session.get(RESPONSES_KEY)
+    if (responses is None):
+        return redirect("/")
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+    if (len(responses)!= qid):
+        return redirect(f"/questions/{len(responses)}")
+    question = survey.questions[qid]
+    return render_template("question.html", question_num = qid, question = question)
+
+@app.route("/complete")
+def complete():
+    return render_template("completion.html")
